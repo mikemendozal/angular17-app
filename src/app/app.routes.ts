824303907a24551157ddf60d40882288fcd5a0b9
Michael <70036874+maik1998@users.noreply.github.com>
2024-06-18 19:36:07 -0500

    Creación del proyecto de angular (Creación de rutas, pagina de login con sus respectivas validaciones,pagina 404, implementación de tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login' , component: LoginComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: '**', component: NotFoundComponent}
+
+];
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,77 @@
+import { Component } from '@angular/core';
+import { FormsModule, NgForm, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  borderColorUser: string = 'border-gray-300';
+  borderFocusColorUser: string = 'focus:border-blue-500';
+  borderColorPassword: string = 'border-gray-300';
+  borderFocusColorPassword: string = 'focus:border-blue-500';
+
+  setBorderColorUser(color: string) {
+    this.borderColorUser = color;
+  }
+
+  setBorderFocusColorUser(color: string) {
+    this.borderFocusColorUser = color;
+  }
+
+  setBorderColorPassword(color: string) {
+    this.borderColorPassword = color;
+  }
+
+  setBorderFocusColorPassword(color: string) {
+    this.borderFocusColorPassword = color;
+  }
+
+  getBorderColorUser() {
+    return this.borderColorUser;
+  }
+
+  getBorderFocusColorUser() {
+    return this.borderFocusColorUser;
+  }
+
+  getBorderColorPassword() {
+    return this.borderColorPassword;
+  }
+
+  getBorderFocusColorPassword() {
+    return this.borderFocusColorPassword;
+  }
+
+  onSubmit(loginForm: NgForm): void {
+    this.getBorderColorUser();
+    this.getBorderFocusColorUser();
+    this.getBorderColorPassword();
+    this.getBorderFocusColorPassword();
+
+    if (loginForm.valid) {
+      if(loginForm.value.username == 'admin' && loginForm.value.password == 'admin'){
+        
+      }
+      console.log('Form Submitted', loginForm.value);
+    } else {
+
+      if(loginForm.controls['usuario'].value === '' && loginForm.controls['password'].value === '') {
+        this.setBorderColorUser('border-red-500');
+        this.setBorderFocusColorUser('focus:border-red-500');
+        this.setBorderColorPassword('border-red-500');
+        this.setBorderFocusColorPassword('focus:border-red-500');
+      }
+
+     
+
+
+      console.error('Form Invalid');
+    }
+  }
+  
+}
